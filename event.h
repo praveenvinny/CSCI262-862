8fc43fa5a128d04dc431f7ed84fbcbff639b4662
pineapple16 <mango16@yeah.net>
2016-10-23 19:51:41 +1100

    Update event.h

--- event.h
+++ event.h
@@ -1,6 +1,7 @@
 #ifndef EVENT_H
 #define EVENT_H
 #include <vector>
+#include <string>
 #define DEBUG
 class ActivityEngine
 {
@@ -8,6 +9,7 @@ class ActivityEngine
         ActivityEngine() : dayCount(0) {}
         ActivityEngine(std::vector<Vehicle> &v): vehicles(v), dayCount(0) {}
         void simulateDay(std::vector<Stats> &stats, std::vector<Vehicle> &vehicles, StatsInfo si);
+        void writeLogFile(std::string outputFileName = "ActivityLog.txt");
     protected:
         void generateVehicle(int offset, std::vector<Stats> stats, std::vector<Vehicle> vehicles, int time);
         void handleParking();
