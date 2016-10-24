b786e8dea2c8077b44615d6457c1f55053c7d3dc
jarod <jarod@debian>
2016-10-24 15:53:02 +1100

    Fixed segfault

--- event.h
+++ event.h
@@ -1,32 +1,44 @@
-/* 
- * File:   event.h
- * Author: Praveen
- *
- * Created on 24 October 2016, 1:33 PM
- */
-
 #ifndef EVENT_H
-#define	EVENT_H
-
+#define EVENT_H
+#include <iostream>
 #include <vector>
+#include <map>
 #include <string>
-#define DEBUG
+//#define DEBUG
 class ActivityEngine
 {
     public:
-        ActivityEngine() : dayCount(0) {}
-        ActivityEngine(std::vector<Vehicle> &v): vehicles(v), dayCount(0) {}
+        ActivityEngine() : dayCount(0)
+        {
+            ofile.open("ActivityLog.txt");
+            if (!ofile.is_open())
+            {
+                std::cerr << "Cannot open file ActivityLog.txt" <<std::endl;
+                exit(-1);
+            }
+            ofile << "Opening log..." << std::endl;
+
+        }
+        ~ActivityEngine()
+        {
+            vehicles.clear();
+            ofile << "Closing log..." << std::endl;
+            ofile.close();
+        }
         void simulateDay(std::vector<Stats> &stats, std::vector<Vehicle> &vehicles, StatsInfo si);
-        void writeLogFile(std::string outputFileName = "ActivityLog", int day = 5);
+        template <class T>
+        void logMessage(T message);
+        void setUpCarList(std::vector<Stats> &stats, std::vector<Vehicle> &vehicles);
     protected:
         void generateVehicle(int offset, std::vector<Stats> stats, std::vector<Vehicle> vehicles, int time);
         void handleParking();
-        void handleDepartures(int time);
+        void handleDepartures(int time,  StatsInfo si);
         void handleMovement();
     private:
         std::vector<Vehicle> vehicles;
+        std::map<std::string, int> carList;
         int dayCount;
+        std::ofstream ofile;
 };
 
-#endif	/* EVENT_H */
-
+#endif /* EVENT_H */
